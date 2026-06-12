2b4211e5c2144187c12f3595d8474e5f2cf072f4
Yusuf-Gadelrab <yusuf.gadelrab06@gmail.com>
2026-06-11 18:25:43 -0700

    Cleanup: move logs to consolidated directory

--- main.py
+++ main.py
@@ -26,7 +26,12 @@ app = FastAPI(title="EdgeLog")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Allow Next.js local dev
+    allow_origins=[
+        "http://localhost:8921",
+        "http://127.0.0.1:8921",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
